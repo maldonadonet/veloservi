c346f4284036f5aa18eb3d93172baa6c1cc3481b
Ruben <desarrollo@ail.com.mx>
2020-03-24 18:04:45 -0600

    Problea con el listado de productos

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage,NavController,NavParams,LoadingController} from "ionic-angular";
 // Provider Users
 import { ApiUsersProvider } from '../../providers/api-users/api-users';
@@ -8,45 +8,59 @@ import { ApiProductsProvider } from '../../providers/api-products/api-products';
 
 @IonicPage()
 @Component({
-    selector: "page-products",
-    templateUrl: "products.html"
+	selector: "page-products",
+	templateUrl: "products.html"
 })
 export class ProductsPage {
-    usuario: any[] = [];
-    productos: any[] = [];
+	usuario: any[] = [];
+	productos: any[] = [];
+	page: number = 1;
+	constructor(public navCtrl: NavController,public navParams: NavParams,public _us: ApiUsersProvider,public _ps:ApiProductsProvider,
+		public loadingCtrl: LoadingController
+	) {
+		this._us.cargar_storage();
+		this.usuario = JSON.parse(this._us.User);
 
-    constructor(
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        private _us: ApiUsersProvider,
-        private _ps: ApiProductsProvider,
-        public loadingCtrl: LoadingController
-    ) {
-        this._us.cargar_storage();
-        this.usuario = JSON.parse(this._us.User);
-    }
+		this._ps.getProducts();
+        
+	}
 
-    ionViewDidLoad() {
-        let loading = this.loadingCtrl.create({
-            content: "Cargando productos..."
-        });
+	ionViewDidLoad() {
+		let loading = this.loadingCtrl.create({
+			content: "Cargando productos..."
+		});
 
-        loading.present();
+		loading.present();
 
-        this._ps.getProducts();
-        this._ps.cargar_storage();
-        loading.dismiss();
-        if (this._ps.productos) {
-            console.log("Productos cargados");
+		this._ps.getProducts();
+		this.productos = this._ps.productos;
+
+		if (this._ps.productos) {
+			console.log("Productos cargados");
             this.productos = this._ps.productos;
-            console.log("Productos_page: ", this.productos);
-        } else {
-            console.log("AUN no tenemos los productos");
-        }
-    }
+            loading.dismiss();
+		} else {
+			console.log("AUN no tenemos los productos");
+		}
+	}
 
-    salir(){
-      this._us.cerrar_sesion();
-      this.navCtrl.push('WelcomePage');
-    }
+	salir() {
+		this._us.cerrar_sesion();
+		this.navCtrl.push("WelcomePage");
+	}
+
+	filtrar_categoria() {
+		this._ps.filtrarCategoria("farmacia");
+		this.productos = this._ps.productos;
+	}
+
+	loadData(event) {
+        console.log("Cargando productos");
+        setTimeout(() => {
+            this._ps.nextPage(2);
+            event.complete();
+            console.log(this.productos);
+        }, 500);
+
+	}
 }
